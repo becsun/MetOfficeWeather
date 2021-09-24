10e8c175e0953a4822bdf936ba01f6fd8dd5a431
Bec Sun <Bec.sun@aexp.com>
2021-09-24 15:53:49 +0100

    BUGgit add .! can\'t get Metoffice API

--- src/main/java/training/metofficeweather/Main.java
+++ src/main/java/training/metofficeweather/Main.java
@@ -1,40 +1,21 @@
 package training.metofficeweather;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.glassfish.jersey.jackson.JacksonFeature;
-import training.metofficeweather.data.loctionInformation.Location;
 import training.metofficeweather.data.loctionInformation.LocationInformation;
-import training.metofficeweather.data.loctionInformation.Locations;
-import training.metofficeweather.data.weatherInformatioin.WeatherReport;
+import training.metofficeweather.data.metOfficeApi.MetOfficeApi;
 import training.metofficeweather.data.weatherInformatioin.Root;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
+
+
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Scanner;
 
 
 public class Main {
-    private static String apiKey = PropertiesReader.getProperty("API_KEY");
-
-    static final String siteUrl = "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=" + apiKey;
-
-    static final Logger theLogger = LogManager.getLogger("Weather");
-
     public static void main(String args[]) {
 
-        theLogger.info("Application Started");
-
-        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
-        Locations retrieveLocations = client.target(siteUrl)
-                .request(MediaType.APPLICATION_JSON)
-                .get(Locations.class);
-
-        theLogger.info("Got {}", retrieveLocations);
+        MetOfficeApi.
 
         ArrayList<LocationInformation> listOfCities = retrieveLocations.getListOfCities();
         listOfCities.forEach(e -> System.out.println(e.getName()));
@@ -44,13 +25,15 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
 
-        String siteUrlForChosenCity = "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/" + cities.get(userInput) + "?res=3hourly&key=" + apiKey;
+
 
         Root retrieveWeather = client.target(siteUrlForChosenCity)
                 .request(MediaType.APPLICATION_JSON)
                 .get(Root.class);
 
         retrieveWeather.weatherReport(userInput, retrieveWeather);
+
+
     }
 
-}	
+}
---
+++ src/main/java/training/metofficeweather/data/metOfficeApi/MetOfficeApi.java
@@ -0,0 +1,53 @@
+package training.metofficeweather.data.metOfficeApi;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import training.metofficeweather.PropertiesReader;
+import training.metofficeweather.data.loctionInformation.Locations;
+import training.metofficeweather.data.weatherInformatioin.Root;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+
+public class MetOfficeApi {
+
+    private static String apiKey = PropertiesReader.getProperty("API_KEY");
+    static final Logger theLogger = LogManager.getLogger("Weather");
+
+    Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+
+    public Locations locationsApiCall(){
+        final String siteUrl = "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=" + apiKey;
+
+        theLogger.info("Application Started");
+
+        Locations retrieveLocations = client.target(siteUrl)
+                .request(MediaType.APPLICATION_JSON)
+                .get(Locations.class);
+
+        theLogger.info("Got {}", retrieveLocations);
+
+        return retrieveLocations;
+    }
+
+
+
+    public Root weatherApiCall(){
+        final String siteUrlForChosenCity = "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/" + cities.get(userInput) + "?res=3hourly&key=" + apiKey;
+
+        theLogger.info("Application Started");
+
+        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+        Root retrieveWeather = client.target(siteUrlForChosenCity)
+                .request(MediaType.APPLICATION_JSON)
+                .get(Root.class);
+
+        theLogger.info("Got {}", retrieveWeather);
+
+        return retrieveWeather;
+    }
+
+}
+
