08ff7d3c899fb0e36dd52f908a5f2cba21895d9f
Borbala Tobias <borbala.tobias@aexp.com>
2021-09-29 10:11:29 +0100

    print more date on the info page

--- src/main/java/training/metofficeweather/WeatherInfo.java
+++ src/main/java/training/metofficeweather/WeatherInfo.java
@@ -1,18 +1,21 @@
 package training.metofficeweather;
 
+import training.metofficeweather.data.weatherInformatioin.DatePeriod;
 import training.metofficeweather.data.weatherInformatioin.WeatherReport;
 
+import java.util.ArrayList;
+
 public class WeatherInfo {
-    private final WeatherReport weatherInformation;
+    private final ArrayList<DatePeriod> weatherInformationForFiveDays;
     private final String cityName;
 
-    public WeatherInfo(WeatherReport weatherInformation, String cityName) {
+    public WeatherInfo(ArrayList<DatePeriod> weatherInformationForFiveDays, String cityName) {
         this.cityName = cityName;
-        this.weatherInformation = weatherInformation;
+        this.weatherInformationForFiveDays = weatherInformationForFiveDays;
     }
 
-    public WeatherReport getWeatherInformation() {
-        return weatherInformation;
+    public ArrayList<DatePeriod> getWeatherInformationForFiveDays() {
+        return weatherInformationForFiveDays;
     }
 
     public String getCityName() {
